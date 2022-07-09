29c7927617b11ee702a81ca8de8edd2d4cade775
David Wong <davidwong.crypto@gmail.com>
2022-07-08 17:58:27 -0700

    added test for public output

--- src/tests/examples.rs
+++ src/tests/examples.rs
@@ -2,17 +2,6 @@ use std::path::Path;
 
 use crate::{ast::Compiler, lexer::Token, parser::AST};
 
-const ARITHMETIC_ASM: &str = "@ noname.0.1.0
-
-DoubleGeneric<1>
-DoubleGeneric<1,1,-1>
-DoubleGeneric<1,0,0,0,-2>
-DoubleGeneric<1,-1>
-(0,0) -> (1,1)
-(1,2) -> (3,0)
-(2,0) -> (3,1)
-";
-
 fn test_file(file_name: &str, expected_gates: usize, expected_asm: &str) {
     let version = env!("CARGO_MANIFEST_DIR");
     let path = Path::new(version).join("data").join(file_name);
@@ -30,5 +19,35 @@ fn test_file(file_name: &str, expected_gates: usize, expected_asm: &str) {
 
 #[test]
 fn test_arithmetic() {
-    test_file("arithmetic.no", 4, ARITHMETIC_ASM);
+    let asm = "@ noname.0.1.0
+
+DoubleGeneric<1>
+DoubleGeneric<1,1,-1>
+DoubleGeneric<1,0,0,0,-2>
+DoubleGeneric<1,-1>
+(0,0) -> (1,1)
+(1,2) -> (3,0)
+(2,0) -> (3,1)
+";
+    test_file("arithmetic.no", 4, asm);
+}
+
+#[test]
+fn test_public_output() {
+    let asm = "@ noname.0.1.0
+
+DoubleGeneric<1>
+DoubleGeneric<1>
+DoubleGeneric<1,1,-1>
+DoubleGeneric<1,0,0,0,-2>
+DoubleGeneric<1,-1>
+DoubleGeneric<1,0,0,0,-6>
+DoubleGeneric<1,1,-1>
+(0,0) -> (2,1)
+(2,2) -> (4,0) -> (6,0)
+(3,0) -> (4,1)
+(5,0) -> (6,1)
+";
+
+    test_file("public_output.no", 7, asm);
 }
