fd7e5d41ab3694b42dd2aeb9bcf463822c993968
mimoo <mimoo@users.noreply.github.com>
2022-09-16 20:47:31 +0000

    deploy: 04f50302bd278afa4f1c69f2438f9c8f2f9821d8

--- rustdoc/field/sidebar-items.js
+++ rustdoc/field/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["add","Adds two field elements"],["equal_vars","This takes variables that can be anything, and returns a boolean"],["sub_cells","Subtracts two cells lhs - rhs"],["sub_vars","Subtracts two variables, we only support variables that are of length 1."]]};
+window.SIDEBAR_ITEMS = {"fn":[["add","Adds two field elements"],["equal","This takes variables that can be anything, and returns a boolean"],["sub","Subtracts two variables, we only support variables that are of length 1."]]};
