c892e90ebe492b97c7e770065086446edbd471ee
mimoo <mimoo@users.noreply.github.com>
2022-10-16 02:17:27 +0000

    deploy: 0787167fa67bbbbec0309654ba49517794b6e81c

---
+++ rustdoc/implementors/serde/de/trait.Deserialize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"noname":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.144/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"noname/inputs/struct.JsonInputs.html\" title=\"struct noname::inputs::JsonInputs\">JsonInputs</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
