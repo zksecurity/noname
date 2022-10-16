3cf2c9ed658bd6e3f1ceca418d641002695d289f
David Wong <davidwong.crypto@gmail.com>
2022-10-16 12:39:07 -0500

    fix bool example asm

--- examples/bool.asm
+++ examples/bool.asm
@@ -1,8 +1,11 @@
 @ noname.0.6.0
 
 DoubleGeneric<1>
+DoubleGeneric<-1,0,0,1>
+DoubleGeneric<-1,0,0,1>
 DoubleGeneric<0,-1,-1,0,1>
 DoubleGeneric<0,-1,0,0,1>
 DoubleGeneric<0,-1,0,0,1>
-(0,0) -> (3,1)
-(1,2) -> (2,1)
+(0,0) -> (1,0) -> (1,1) -> (5,1)
+(2,0) -> (2,1) -> (3,1)
+(3,2) -> (4,1)
