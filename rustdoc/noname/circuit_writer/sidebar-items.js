61b43c31ad4104ae124c1eb222b5ed338f3d4aaa
mimoo <mimoo@users.noreply.github.com>
2022-10-23 01:30:00 +0000

    deploy: 46a23616c9a28ae268583f51b13fe3756e8ea932

--- rustdoc/noname/circuit_writer/sidebar-items.js
+++ rustdoc/noname/circuit_writer/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["GateKind",""],["VarOrRef","Represents a variable in the circuit, or a reference to one. Note that mutable variables are always passed as references, as one needs to have access to the variable name to be able to reassign it in the environment."],["Wiring",""]],"mod":[["fn_env",""]],"struct":[["Cell",""],["CircuitWriter",""],["Gate",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["fn_env",""],["writer",""]],"struct":[["CircuitWriter",""]]};
---
+++ rustdoc/noname/circuit_writer/writer/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["GateKind",""],["Wiring",""]],"struct":[["Cell",""],["Gate",""]]};
