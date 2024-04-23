e2ad54ccb92834ea464ffdd03ca3a93794776cfe
kata <kata.choi@gmail.com>
2024-04-23 03:12:50 +0000

    test: update asm

--- examples/equals.asm
+++ examples/equals.asm
@@ -7,34 +7,34 @@ DoubleGeneric<1,1,-1>
 DoubleGeneric<1,0,1>
 DoubleGeneric<1,0,-1,0,1>
 DoubleGeneric<0,0,-1,1>
+DoubleGeneric<1>
 DoubleGeneric<0,0,-1,1>
-DoubleGeneric<1,0,1>
-DoubleGeneric<1,1,-1>
-DoubleGeneric<0,-1,0,0,1>
+DoubleGeneric<1,-1>
+DoubleGeneric<1,0,0,0,-1>
 DoubleGeneric<1,0,0,0,-3>
 DoubleGeneric<1,0,1>
 DoubleGeneric<1,1,-1>
 DoubleGeneric<1,0,1>
 DoubleGeneric<1,0,-1,0,1>
 DoubleGeneric<0,0,-1,1>
+DoubleGeneric<1>
 DoubleGeneric<0,0,-1,1>
-DoubleGeneric<1,0,1>
-DoubleGeneric<1,1,-1>
-DoubleGeneric<0,-1,0,0,1>
+DoubleGeneric<1,-1>
+DoubleGeneric<1,0,0,0,-1>
 (0,0) -> (2,0) -> (12,0)
 (1,0) -> (3,0)
 (2,2) -> (3,1)
-(3,2) -> (6,1) -> (7,1)
-(4,0) -> (6,0) -> (10,1)
+(3,2) -> (6,1) -> (8,1)
+(4,0) -> (6,0) -> (10,0)
 (4,2) -> (5,0)
-(5,2) -> (8,0)
-(7,2) -> (9,0)
-(8,2) -> (9,1)
+(5,2) -> (9,1)
+(6,2) -> (7,0)
+(8,2) -> (9,0)
 (11,0) -> (13,0)
 (12,2) -> (13,1)
-(13,2) -> (16,1) -> (17,1)
-(14,0) -> (16,0) -> (20,1)
+(13,2) -> (16,1) -> (18,1)
+(14,0) -> (16,0) -> (20,0)
 (14,2) -> (15,0)
-(15,2) -> (18,0)
-(17,2) -> (19,0)
-(18,2) -> (19,1)
+(15,2) -> (19,1)
+(16,2) -> (17,0)
+(18,2) -> (19,0)
--- examples/if_else.asm
+++ examples/if_else.asm
@@ -8,9 +8,9 @@ DoubleGeneric<1,1,-1>
 DoubleGeneric<1,0,1>
 DoubleGeneric<1,0,-1,0,1>
 DoubleGeneric<0,0,-1,1>
+DoubleGeneric<1>
 DoubleGeneric<0,0,-1,1>
-DoubleGeneric<1,0,1>
-DoubleGeneric<1,1,-1>
+DoubleGeneric<1,-1>
 DoubleGeneric<1,0,1>
 DoubleGeneric<1,1,-1>
 DoubleGeneric<1,0,1>
@@ -23,12 +23,12 @@ DoubleGeneric<1,0,0,0,-2>
 (1,2) -> (12,0)
 (2,0) -> (4,0)
 (3,2) -> (4,1)
-(4,2) -> (7,1) -> (8,1)
+(4,2) -> (7,1) -> (9,1)
 (5,0) -> (7,0) -> (15,0)
 (5,2) -> (6,0)
-(6,2) -> (9,0)
-(8,2) -> (10,0)
-(9,2) -> (10,1)
+(6,2) -> (10,1)
+(7,2) -> (8,0)
+(9,2) -> (10,0)
 (11,2) -> (12,1)
 (12,2) -> (15,1)
 (13,2) -> (14,1)
