e4cd10f5cd112a8b66488af61c61162d2c12efbb
kata <kata.choi@gmail.com>
2024-04-23 08:42:59 +0000

    revert test

--- examples/arithmetic.asm
+++ examples/arithmetic.asm
@@ -3,11 +3,5 @@
 DoubleGeneric<1>
 DoubleGeneric<1,1,-1>
 DoubleGeneric<1,0,0,0,-2>
-DoubleGeneric<1,0,1>
-DoubleGeneric<1,1,-1>
-DoubleGeneric<1>
-(0,0) -> (1,1) -> (3,0)
-(1,0) -> (4,0)
+(0,0) -> (1,1)
 (1,2) -> (2,0)
-(3,2) -> (4,1)
-(4,2) -> (5,0)
