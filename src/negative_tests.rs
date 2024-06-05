a5bff39d9050562069858eac0ab5eeee703b962f
Vu Vo <vu.voth@gmail.com>
2024-06-05 19:54:48 +0700

    macro for test negative

--- src/negative_tests.rs
+++ src/negative_tests.rs
@@ -1,75 +1,71 @@
-use crate::{
-    backends::kimchi::KimchiVesta,
-    compiler::{typecheck_next_file_inner, Sources},
-    error::ErrorKind,
-    type_checker::TypeChecker,
-};
+macro_rules! create_test_with_error {
+    ($test_name: ident, $expected_error: pat, $code: expr) => {
+        #[test]
+        fn $test_name() {
+            let mut tast = TypeChecker::<KimchiVesta>::new();
+            let res = typecheck_next_file_inner(
+                &mut tast,
+                None,
+                &mut Sources::new(),
+                "example.no".to_string(),
+                $code.to_string(),
+                0,
+            );
+            let actual_error = res.unwrap_err().kind;
 
-#[test]
-fn test_return() {
-    // no return expected
-    let code = r#"
+            assert!(matches!(actual_error, $expected_error));
+
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        backends::kimchi::KimchiVesta,
+        compiler::{typecheck_next_file_inner, Sources},
+        error::ErrorKind,
+        type_checker::TypeChecker,
+    };
+
+    create_test_with_error!(
+        test_return,
+        ErrorKind::NoReturnExpected,
+        r#"
     fn thing(xx: Field) {
         return xx;
     }
-    "#;
-
-    let mut tast = TypeChecker::<KimchiVesta>::new();
-    let res = typecheck_next_file_inner(
-        &mut tast,
-        None,
-        &mut Sources::new(),
-        "example.no".to_string(),
-        code.to_string(),
-        0,
+    "#
     );
 
-    assert!(matches!(res.unwrap_err().kind, ErrorKind::NoReturnExpected));
-}
-
-#[test]
-fn test_return_expected() {
-    // return expected
-    let code = r#"
+    create_test_with_error!(
+        test_return_expected,
+        ErrorKind::MissingReturn,
+        r#"
     fn thing(xx: Field) -> Field {
         let yy = xx + 1;
-    }
-    "#;
-
-    let mut tast = TypeChecker::<KimchiVesta>::new();
-    let res = typecheck_next_file_inner(
-        &mut tast,
-        None,
-        &mut Sources::new(),
-        "example.no".to_string(),
-        code.to_string(),
-        0,
+    } 
+    "#
     );
 
-    assert!(matches!(res.unwrap_err().kind, ErrorKind::MissingReturn));
-}
-
-#[test]
-fn test_return_mismatch() {
-    // return type mismatch
-    let code = r#"
-        fn thing(xx: Field) -> Field {
-            return true;
-        }
-        "#;
-
-    let mut tast = TypeChecker::<KimchiVesta>::new();
-    let res = typecheck_next_file_inner(
-        &mut tast,
-        None,
-        &mut Sources::new(),
-        "example.no".to_string(),
-        code.to_string(),
-        0,
+    create_test_with_error!(
+        return_mismatch,
+        ErrorKind::ReturnTypeMismatch(..),
+        r#"
+    fn thing(xx: Field) -> Field {
+        return true;
+    } 
+    "#
     );
 
-    assert!(matches!(
-        res.unwrap_err().kind,
-        ErrorKind::ReturnTypeMismatch(..)
-    ));
+    create_test_with_error!(
+        duplicate_definition,
+        ErrorKind::DuplicateDefinition(_),
+        r#"
+    fn thing(xx: Field) -> Field {
+        let xx = 10;
+        let xx = 12;
+    } 
+    "#
+    );
 }
