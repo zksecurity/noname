681d24a17a9a1e2c3bfc67d96b2a61cb96c8487e
Vu Vo <vu.voth@gmail.com>
2024-06-17 18:27:16 +0700

    revert change and use should panic

--- src/negative_tests.rs
+++ src/negative_tests.rs
@@ -1,23 +1,3 @@
-macro_rules! create_test_with_error {
-    ($test_name: ident, $expected_error: pat, $code: expr) => {
-        #[test]
-        fn $test_name() {
-            let mut tast = TypeChecker::<KimchiVesta>::new();
-            let res = typecheck_next_file_inner(
-                &mut tast,
-                None,
-                &mut Sources::new(),
-                "example.no".to_string(),
-                $code.to_string(),
-                0,
-            );
-            let actual_error = res.unwrap_err().kind;
-
-            assert!(matches!(actual_error, $expected_error));
-        }
-    };
-}
-
 #[cfg(test)]
 mod tests {
     use crate::{
@@ -27,44 +7,66 @@ mod tests {
         type_checker::TypeChecker,
     };
 
-    create_test_with_error!(
-        test_return,
-        ErrorKind::NoReturnExpected,
-        r#"
-    fn thing(xx: Field) {
-        return xx;
+    fn run_type_check_for_invalid_code(code: &str) -> crate::error::Result<usize> {
+        let mut tast = TypeChecker::<KimchiVesta>::new();
+        typecheck_next_file_inner(
+            &mut tast,
+            None,
+            &mut Sources::new(),
+            "example.no".to_string(),
+            code.to_string(),
+            0,
+        )
     }
-    "#
-    );
 
-    create_test_with_error!(
-        test_return_expected,
-        ErrorKind::MissingReturn,
-        r#"
-    fn thing(xx: Field) -> Field {
-        let yy = xx + 1;
-    } 
-    "#
-    );
+    #[test]
+    #[should_panic(expected = "NoReturnExpected")]
+    fn test_return() {
+        // no return expected
+        let code = r#"
+        fn thing(xx: Field) {
+            return xx;
+        }
+        "#;
+        run_type_check_for_invalid_code(code).unwrap();
+    }
 
-    create_test_with_error!(
-        return_mismatch,
-        ErrorKind::ReturnTypeMismatch(..),
-        r#"
-    fn thing(xx: Field) -> Field {
-        return true;
-    } 
-    "#
-    );
+    #[test]
+    #[should_panic(expected = "MissingReturn")]
+    fn test_return_expected() {
+        // return expected
+        let code = r#"
+        fn thing(xx: Field) -> Field {
+            let yy = xx + 1;
+        }
+        "#;
 
-    create_test_with_error!(
-        duplicate_definition,
-        ErrorKind::DuplicateDefinition(_),
-        r#"
+        run_type_check_for_invalid_code(code).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "ReturnTypeMismatch(Field, Bool)")]
+    fn test_return_mismatch() {
+        // return type mismatch
+        let code = r#"
+        fn thing(xx: Field) -> Field {
+            return true;
+        }
+        "#;
+
+        run_type_check_for_invalid_code(code).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "DuplicateDefinition(\"xx\")")]
+    fn test_duplicate_definition() {
+        let code = r#"
     fn thing(xx: Field) -> Field {
         let xx = 10;
         let xx = 12;
     } 
-    "#
-    );
+    "#;
+
+        run_type_check_for_invalid_code(code).unwrap();
+    }
 }
