c60b3868a37c653cefcf1e4b9c619c84aff8d5fe
Prabhat1308 <prabhatverma329@gmail.com>
2024-07-31 16:19:40 +0530

    add test pointing to dup_var.no
    Signed-off-by: Prabhat1308 <prabhatverma329@gmail.com>

---
+++ examples/fixture/asm/kimchi/dup_var.asm
@@ -0,0 +1,18 @@
+@ noname.0.7.0
+
+DoubleGeneric<1>
+DoubleGeneric<1,1,-1>
+DoubleGeneric<1,1,-1>
+DoubleGeneric<1,1,-1>
+DoubleGeneric<1,1,-1>
+DoubleGeneric<1,1,-1>
+DoubleGeneric<1,-1>
+(0,0) -> (6,1)
+(1,0) -> (3,1)
+(1,1) -> (4,1)
+(1,2) -> (2,0)
+(2,1) -> (5,1)
+(2,2) -> (3,0)
+(3,2) -> (4,0)
+(4,2) -> (5,0)
+(5,2) -> (6,0)
