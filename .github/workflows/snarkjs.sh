4971dace7c9e40ac124872789d33516c4e7ca244
ronantakizawa <ronantakizawa@gmail.com>
2024-08-28 10:18:40 -0600

    fix: use sudoku example, download ptau file from external source

--- .github/workflows/snarkjs.sh
+++ .github/workflows/snarkjs.sh
@@ -8,32 +8,106 @@ fi
 
 DIR_PATH=$1
 CURVE=$2
-NEW_PTAU=false
-
-# Check for optional setup flag
-for arg in "$@"; do
-    if [[ $arg == "--new" ]]; then
-        NEW_PTAU=true
-    fi
-done
 
 # Ensure the circuit directory exists and is initialized
 echo "Initializing a new Noname package..."
 noname new --path circuit_noname
 
+# Write the Sudoku example to main.no
+echo "Writing Sudoku circuit code to main.no..."
+cat > "$DIR_PATH/circuit_noname/src/main.no" <<EOL
+const empty = 0;
+const player1 = 1;
+const player2 = 2;
+const sudoku_size = 81; // 9 * 9
+
+struct Sudoku {
+    inner: [Field; 81],
+}
+
+// return the value in a given cell
+fn Sudoku.cell(self, const row: Field, const col: Field) -> Field {
+    return self.inner[(row * 9) + col];
+}
+
+// verifies that self matches the grid in places where the grid has numbers
+fn Sudoku.matches(self, grid: Sudoku) {
+    // for each cell
+    for row in 0..9 {
+        for col in 0..9 {
+            // either the solution matches the grid
+            // or the grid is zero
+            let matches = self.cell(row, col) == grid.cell(row, col);
+            let is_empty = grid.cell(row, col) == empty;
+            assert(matches || is_empty);
+        }
+    }
+}
+
+fn Sudoku.verify_rows(self) {
+    for row in 0..9 {
+        for num in 1..10 {
+            let mut found = false;
+            for col in 0..9 {
+                let found_one = self.cell(row, col) == num;
+                found = found || found_one;
+            }
+            assert(found);
+        }
+    }
+}
+
+fn Sudoku.verify_cols(self) {
+    for col in 0..9 {
+        for num in 1..10 {
+            let mut found = false;
+            for row in 0..9 {
+                let found_one = self.cell(row, col) == num;
+                found = found || found_one;
+            }
+            assert(found);
+        }
+    }
+}
+
+fn Sudoku.verify_diagonals(self) {
+    for num in 1..10 {
+
+        // first diagonal
+        let mut found1 = false;
+        for row1 in 0..9 {
+            let temp1 = self.cell(row1, row1) == num;
+            found1 = found1 || temp1;
+        }
+        assert(found1);
+
+        // second diagonal
+        let mut found2 = false;
+        for row2 in 0..9 {
+            let temp2 = self.cell(8 - row2, row2) == num;
+            found2 = found2 || temp2;
+        }
+        assert(found2);
+    }
+}
+
+fn Sudoku.verify(self) {
+    self.verify_rows();
+    self.verify_cols();
+    self.verify_diagonals();
+}
+
+fn main(pub grid: Sudoku, solution: Sudoku) {
+    solution.matches(grid);
+    solution.verify();
+}
+EOL
+
 # Compile the circuit using Noname CLI
 echo "Compiling the circuit using Noname..."
-noname run --backend r1cs-$CURVE --path "circuit_noname" \
---public-inputs '{"xx": "5"}' \
---private-inputs '{"yy": "4"}'
-
-# Generate a proof and verify it via snarkjs
-if $NEW_PTAU; then
-    echo "Generating new powers of tau..."
-    snarkjs powersoftau new $CURVE 16 "pot_${CURVE}_0000.ptau" -v
-    echo "your-random-text" | snarkjs powersoftau contribute "pot_${CURVE}_0000.ptau" "pot_${CURVE}_0001.ptau" --name="First contribution" -v
-    snarkjs powersoftau prepare phase2 "pot_${CURVE}_0001.ptau" "pot_${CURVE}_final.ptau" -v
-fi
+noname run --backend r1cs-bn254 --path $DIR_PATH/circuit_noname \
+--private-inputs '{"solution": { "inner": ["9", "5", "3", "6", "2", "1", "7", "8", "4", "1", "4", "8", "7", "5", "9", "2", "6", "3", "2", "7", "6", "8", "3", "4", "9", "5", "1", "3", "6", "9", "2", "7", "5", "4", "1", "8", "4", "8", "5", "9", "1", "6", "3", "7", "2", "7", "1", "2", "3", "4", "8", "6", "9", "5", "6", "3", "7", "1", "8", "2", "5", "4", "9", "5", "2", "1", "4", "9", "7", "8", "3", "6", "8", "9", "4", "5", "6", "3", "1", "2", "7"] }}' \
+--public-inputs '{"grid": { "inner": ["0", "5", "3", "6", "2", "1", "7", "8", "4", "0", "4", "8", "7", "5", "9", "2", "6", "3", "2", "7", "6", "8", "3", "4", "9", "5", "1", "3", "6", "9", "2", "7", "0", "4", "1", "8", "4", "8", "5", "9", "1", "6", "3", "7", "2", "0", "1", "2", "3", "4", "8", "6", "9", "5", "6", "3", "0", "1", "8", "2", "5", "4", "9", "5", "2", "1", "4", "9", "0", "8", "3", "6", "8", "9", "4", "5", "6", "3", "1", "2", "7"] }}'
 
 # Run snarkjs witness check to ensure that the witness is correctly generated
 echo "Running snarkjs wchk..."
@@ -41,7 +115,7 @@ snarkjs wchk "$DIR_PATH/circuit_noname/output.r1cs" "$DIR_PATH/circuit_noname/ou
 
 # Setup the Groth16 proving system using the R1CS and PTau files
 echo "Running snarkjs groth16 setup..."
-snarkjs groth16 setup "$DIR_PATH/circuit_noname/output.r1cs" "pot_${CURVE}_final.ptau" "test_${CURVE}_0000.zkey"
+snarkjs groth16 setup "$DIR_PATH/circuit_noname/output.r1cs" "pot_bn254_final.ptau" "test_${CURVE}_0000.zkey"
 
 # Contribute to the zkey file for Groth16
 echo "Running snarkjs zkey contribute..."
