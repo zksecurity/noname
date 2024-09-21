1b027e8547d323b84e9c0eb5394830746ea9ea64
kata <kata.choi@gmail.com>
2024-09-21 17:14:10 +0800

    add tests

---
+++ examples/fixture/asm/kimchi/generic_nested_func.asm
@@ -0,0 +1,13 @@
+@ noname.0.7.0
+
+DoubleGeneric<1>
+DoubleGeneric<1>
+DoubleGeneric<1>
+DoubleGeneric<1>
+DoubleGeneric<1,-1>
+DoubleGeneric<1,-1>
+DoubleGeneric<1,-1>
+(0,0) -> (4,0)
+(1,0) -> (5,0)
+(2,0) -> (6,0)
+(3,0) -> (4,1) -> (5,1) -> (6,1)
