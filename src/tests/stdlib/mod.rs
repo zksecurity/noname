6f71efc7d6f263ed6d85d39e85c68fb7de04e409
kata <kata.choi@gmail.com>
2024-09-27 16:53:20 +0800

    incorporate native stdlib for testing

---
+++ src/tests/stdlib/mod.rs
@@ -0,0 +1,94 @@
+mod bigint;
+mod comparator;
+
+use std::{path::Path, str::FromStr};
+
+use crate::{
+    backends::r1cs::{R1csBn254Field, R1CS},
+    circuit_writer::CircuitWriter,
+    compiler::{typecheck_next_file, Sources},
+    error::Result,
+    inputs::parse_inputs,
+    mast,
+    tests::init_stdlib_dep,
+    type_checker::TypeChecker,
+    witness::CompiledCircuit,
+};
+
+fn test_stdlib(
+    path: &str,
+    asm_path: &str,
+    public_inputs: &str,
+    private_inputs: &str,
+    expected_public_output: Vec<&str>,
+) -> Result<CompiledCircuit<R1CS<R1csBn254Field>>> {
+    let r1cs = R1CS::new();
+    let root = env!("CARGO_MANIFEST_DIR");
+    let prefix_path = Path::new(root).join("src/tests/stdlib");
+
+    // read noname file
+    let code = std::fs::read_to_string(prefix_path.clone().join(path)).unwrap();
+
+    // parse inputs
+    let public_inputs = parse_inputs(public_inputs).unwrap();
+    let private_inputs = parse_inputs(private_inputs).unwrap();
+
+    // compile
+    let mut sources = Sources::new();
+    let mut tast = TypeChecker::new();
+    init_stdlib_dep(&mut sources, &mut tast);
+
+    let this_module = None;
+    let _node_id = typecheck_next_file(
+        &mut tast,
+        this_module,
+        &mut sources,
+        path.to_string(),
+        code.clone(),
+        0,
+    )
+    .unwrap();
+
+    let mast = mast::monomorphize(tast)?;
+    let compiled_circuit = CircuitWriter::generate_circuit(mast, r1cs)?;
+
+    // this should check the constraints
+    let generated_witness = compiled_circuit
+        .generate_witness(public_inputs.clone(), private_inputs.clone())
+        .unwrap();
+
+    let expected_public_output = expected_public_output
+        .iter()
+        .map(|x| crate::backends::r1cs::R1csBn254Field::from_str(x).unwrap())
+        .collect::<Vec<_>>();
+
+    if generated_witness.outputs != expected_public_output {
+        eprintln!("obtained by executing the circuit:");
+        generated_witness
+            .outputs
+            .iter()
+            .for_each(|x| eprintln!("- {x}"));
+        eprintln!("passed as output by the verifier:");
+        expected_public_output
+            .iter()
+            .for_each(|x| eprintln!("- {x}"));
+        panic!("Obtained output does not match expected output");
+    }
+
+    // check the ASM
+    if compiled_circuit.circuit.backend.num_constraints() < 100 {
+        let prefix_asm = Path::new(root).join("src/tests/stdlib/");
+        let expected_asm = std::fs::read_to_string(prefix_asm.clone().join(asm_path)).unwrap();
+        let obtained_asm = compiled_circuit.asm(&Sources::new(), false);
+
+        if obtained_asm != expected_asm {
+            eprintln!("obtained:");
+            eprintln!("{obtained_asm}");
+            eprintln!("expected:");
+            eprintln!("{expected_asm}");
+            panic!("Obtained ASM does not match expected ASM");
+        }
+    }
+
+    Ok(compiled_circuit)
+}
