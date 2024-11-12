3dc4c71ff2b4b34975d62a40b58d6076116eb934
kata <kata.choi@gmail.com>
2024-11-12 17:31:51 +0800

    update tests

--- examples/fixture/asm/kimchi/hint.asm
+++ examples/fixture/asm/kimchi/hint.asm
@@ -12,9 +12,12 @@ DoubleGeneric<1,-1>
 DoubleGeneric<1,0,0,0,-16>
 DoubleGeneric<1,-1>
 DoubleGeneric<1,0,0,0,-3>
+DoubleGeneric<1,0,-1,0,1>
+DoubleGeneric<1,0,0,0,-1>
 DoubleGeneric<1,-1>
-(0,0) -> (11,0)
+(0,0) -> (13,0)
 (1,0) -> (2,0) -> (9,1)
 (4,0) -> (6,1)
 (4,2) -> (5,1)
-(5,0) -> (7,1) -> (11,1)
+(5,0) -> (7,1) -> (13,1)
+(9,0) -> (11,0)
