a25a26b65e0b66d0f9bda5fc92208277fe7cacad
katat <katat@users.noreply.github.com>
2025-01-14 12:50:28 +0000

    deploy: 2d76977e6c26936f21f2d81f1b53e85fe1dd84d8

--- rustdoc/noname/stdlib/builtins/sidebar-items.js
+++ rustdoc/noname/stdlib/builtins/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["BUILTIN_FN_NAMES","QUALIFIED_BUILTINS"],"struct":["BuiltinsLib"]};
+window.SIDEBAR_ITEMS = {"constant":["BUILTIN_FN_NAMES","QUALIFIED_BUILTINS"],"struct":["BuiltinsLib"],"trait":["Builtin"]};
